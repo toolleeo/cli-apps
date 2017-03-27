908aa2d240cc3ad096d56934af1aeb3715e1e07a
Tullio Facchinetti <tullio.facchinetti@unipv.it>
2017-03-27 22:10:11 +0200

    Added some features to the bare Markdown generator.

--- cli2md.py
+++ cli2md.py
@@ -5,11 +5,18 @@ def print_app(app):
     print("* [{}]({}) - {}".format(app["name"], app["url"], app["description"]))
 
 
+def count_apps(apps):
+    tot = 0
+    for cat in apps:
+        tot += len(apps[cat])
+    return tot
+
 with open('cli-apps.yaml', 'r') as yf:
     data = load(yf)
     # print(data)
     apps = data["apps"]
     for cat in apps:
-        print(cat)
+        print("\n{}\n".format(cat))
         for app in apps[cat]:
             print_app(app)
+    print("\nTotal: {} apps.".format(count_apps(apps)))
